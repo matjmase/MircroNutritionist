2364b606c29ce1e5326fbc6b1715c06d7369644f
matjmase <67738268+matjmase@users.noreply.github.com>
2023-09-27 12:00:55 -0500

    Refactoring and View Adjustment

---
+++ MicroNutritionist/MicroNutritionist/Views/SubViews/Crud/NutritionAmountRowView.xaml.cs
@@ -0,0 +1,57 @@
+using System.Windows.Input;
+
+namespace MicroNutritionist.Views.SubViews.Crud;
+
+public partial class NutritionAmountRowView : ContentView
+{
+    public static readonly BindableProperty MainObjectProperty =
+  BindableProperty.Create(nameof(MainObject), typeof(object), typeof(NutritionAmountRowView));
+
+    public object MainObject
+    {
+        get => GetValue(MainObjectProperty) as object;
+        set => SetValue(MainObjectProperty, value);
+    }
+
+
+    public static readonly BindableProperty MainNameProperty =
+  BindableProperty.Create(nameof(MainName), typeof(string), typeof(NutritionAmountRowView));
+
+    public string MainName
+    {
+        get => GetValue(MainNameProperty) as string;
+        set => SetValue(MainNameProperty, value);
+    }
+
+
+    public static readonly BindableProperty MainIntegerProperty =
+  BindableProperty.Create(nameof(MainInteger), typeof(int), typeof(NutritionAmountRowView));
+
+    public int? MainInteger
+    {
+        get => GetValue(MainIntegerProperty) as int?;
+        set => SetValue(MainIntegerProperty, value);
+    }
+
+    public static readonly BindableProperty RemoveItemProperty =
+  BindableProperty.Create(nameof(RemoveItem), typeof(ICommand), typeof(NutritionAmountRowView));
+
+    public ICommand RemoveItem
+    {
+        get => GetValue(RemoveItemProperty) as ICommand;
+        set => SetValue(RemoveItemProperty, value);
+    }
+
+    public static readonly BindableProperty EditItemProperty =
+  BindableProperty.Create(nameof(EditItem), typeof(ICommand), typeof(NutritionAmountRowView));
+
+    public ICommand EditItem
+    {
+        get => GetValue(EditItemProperty) as ICommand;
+        set => SetValue(EditItemProperty, value);
+    }
+    public NutritionAmountRowView()
+	{
+		InitializeComponent();
+	}
+}
